a839347a9b7af4407c3884105cfbcc09f678f508
Larissa Hilario Clemente <larihid1@gmail.com>
2023-11-06 08:47:26 -0600

    feat: integration front-back

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Home from './pages/Home'
+import './App.css';
+import CodeEditor from './pages/Home';
+function App() {
+
+  return (
+    <>
+      <div className="App">
+      <CodeEditor/>
+    </div>
+    </>
+  )
+}
+
+export default App
